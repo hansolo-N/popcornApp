6960692ecbc6d01e0bc549b811dac0fa7b2b539b
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-09-20 18:15:36 +0200

    fixed hover out

--- src/components/rating/Rating.jsx
+++ src/components/rating/Rating.jsx
@@ -1,18 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
 
-
-
-function Rating({numStars}) {
-const [rating,setRating] = useState(0)
-const [tempRating,setTempRating] = useState(0)
-
-function HandleRating (rating){
-  setRating(rating)
-
-}
-
-
 const containerStyle = {
   display: "flex",
   alignItems: 'center',
@@ -25,51 +13,60 @@ const starContainer = {
   gap: "4px"
 }
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0"
-
-
-}
-  return (
-    <div style={containerStyle}>
-      <div style={starContainer}>
-      {Array.from({length:numStars},(_,i)=>
-      
-      <Star 
-       key={i}
-       onRate={()=>HandleRating(i+1)} 
-       full = {tempRating?tempRating>= i+1:rating >=i+1}
-       onHoverIn ={()=>setTempRating(i+1)}
-       onHoverOut ={()=>setTempRating(0)}
-       />
-      
-      )}
-      </div>
-      <p style={textStyle}>{tempRating ||""}</p>
-    </div>
-  )
-}
-
-const starStyle = {
-  height:"48px",
-  width: "48px",
-  display: "block",
-  cursor: "pointer"
-}
-
-
-function Star ({onRate,full,onHoverIn,onHoverOut})
+function Rating({numStars})
 {
-  return(
-<span 
-role="button"
-style={starStyle}
-onClick={onRate}
-onMouseEnter={onHoverIn}
-onMouseDownCapture={onHoverOut}
->
-{full?
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
+
+  function handleRating(rating) {
+    setRating(rating);
+  }
+
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+
+  };
+
+  return (
+    <div style={containerStyle} className="">
+      <div style={starContainer}>
+        {Array.from({ length: numStars }, (_, i) => (
+          <Star
+            key={i}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onRate={() => handleRating(i + 1)}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+          />
+        ))}
+      </div>
+      <p style={textStyle}>
+        {tempRating || rating || ""}
+      </p>
+    </div>
+  );
+}
+
+export default Rating
+
+function Star({ onRate, full, onHoverIn, onHoverOut}) {
+  const starStyle = {
+    width: `48px`,
+    height: `48px`,
+    display: "block",
+    cursor: "pointer",
+  };
+
+  return (
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
+      {full?
   <svg
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 20 20"
@@ -93,11 +90,6 @@ onMouseDownCapture={onHoverOut}
     d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
   />
 </svg>}
-
-</span>
-  )
+    </span>
+  );
 }
-
-
-
-export default Rating
