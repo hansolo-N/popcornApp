71fabae9f14ae402f2a0859b04b84f6d56523792
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-09-22 16:23:09 +0200

    added use effect to fetch movie data search

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Rating from './components/rating/Rating';
+import App from './App-v1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Rating numStars={5} messages ={['horrible','ok','good','great','amazing']} className='' defaultRating={0} onSetRating/>
+    <App />
   </React.StrictMode>
 );
